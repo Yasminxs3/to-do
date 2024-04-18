02f81c8cd101d4d25fea484bb95670b2d748d1da
yasminxs3 <yasminxs3@gmail.com>
2024-04-17 22:03:16 -0300

    remove unnecessary import

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { ChangeEvent, FormEvent, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import styles from "./App.module.css";
 import "./global.css";
 import { Header } from "./components/Header";
